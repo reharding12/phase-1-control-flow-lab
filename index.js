ffcd7ccddc9d9c7922d91cdb7a74c904e3b62055
Roxanne <reharding12@gmail.com>
2024-02-01 19:14:04 -0700

    passed 9 tests

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let freeRide;
+  if (feet <= 400) {
+    freeRide = 'This one is on me!';
+  }
+  if (feet > 400 && feet < 2000) {
+    freeRide = 'That will be twenty bucks.';
+  }
+  if (feet > 2000) {
+    freeRide = 'I will gladly take your thirty bucks.';
+  }
+  if (feet > 2500) {
+    freeRide = 'No can do.';
+  }
+return freeRide;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let area;
+  city === 'NYC' ? (area = 'Ok, sounds good.') : (area = 'No go.');
+  
+return area;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
